0f09ef53a7a724ef5018fe7e208d73b2b8a7d0fd
Martin Maartensson <martinmaartensson@gmail.com>
2025-06-17 15:05:50 +0200

    change name to nxvi

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = {self, nixvim, flake-utils, ...}:
   flake-utils.lib.eachDefaultSystem (system: {
     nixosModules.default = {config, lib, pkgs, ...}: {
-      options.programs.mvim = {
+      options.programs.nxvi = {
         enable = lib.mkEnableOption "Enable nixvim editor";
         environments = {
           golang.enable = lib.mkEnableOption "Enable golang environment";
@@ -24,22 +24,22 @@
         };
       };
 
-      config = lib.mkIf config.programs.mvim.enable {
+      config = lib.mkIf config.programs.nxvi.enable {
         environment.systemPackages =  [
           (nixvim.legacyPackages.${system}.makeNixvimWithModule {
             inherit system;
             module = [
               ./base_config
             ]
-            ++ lib.optional config.programs.mvim.environments.golang.enable ./environments/golang
-            ++ lib.optional config.programs.mvim.environments.rust.enable ./environments/rust
-            ++ lib.optional config.programs.mvim.environments.python.enable ./environments/python
-            ++ lib.optional config.programs.mvim.environments.kotlin.enable ./environments/kotlin
-            ++ lib.optional config.programs.mvim.environments.haskell.enable ./environments/haskell
-            ++ lib.optional config.programs.mvim.environments.web.enable ./environments/webdev
-            ++ lib.optional config.programs.mvim.environments.plantuml.enable ./environments/plantuml
-            ++ lib.optional config.programs.mvim.environments.java.enable ./environments/java
-            ++ lib.optional config.programs.mvim.environments.database.enable ./environments/database;
+            ++ lib.optional config.programs.nxvi.environments.golang.enable ./environments/golang
+            ++ lib.optional config.programs.nxvi.environments.rust.enable ./environments/rust
+            ++ lib.optional config.programs.nxvi.environments.python.enable ./environments/python
+            ++ lib.optional config.programs.nxvi.environments.kotlin.enable ./environments/kotlin
+            ++ lib.optional config.programs.nxvi.environments.haskell.enable ./environments/haskell
+            ++ lib.optional config.programs.nxvi.environments.web.enable ./environments/webdev
+            ++ lib.optional config.programs.nxvi.environments.plantuml.enable ./environments/plantuml
+            ++ lib.optional config.programs.nxvi.environments.java.enable ./environments/java
+            ++ lib.optional config.programs.nxvi.environments.database.enable ./environments/database;
           })
         ];
 
