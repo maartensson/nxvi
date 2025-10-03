ab0112061f267b1bbd33e67e3b6cec9ee0351f19
Martin Maartensson <martinmaartensson@gmail.com>
2025-10-03 23:28:30 +0200

    add rust_analyzer

--- environments/rust/plugins.nix
+++ environments/rust/plugins.nix
@@ -1,3 +1,14 @@
 {
   rustaceanvim.enable = true;
+  lsp = {
+    enable = true;
+    servers = {
+      rust_analyzer = {
+        enable = true;
+        settings = {
+          procMacro.enable = false;
+        };
+      };
+    };
+  };
 }
