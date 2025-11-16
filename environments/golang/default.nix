c2fd95b3b3f01e8b5faab5031faf0788be3cca69
Martin Maartensson <martinmaartensson@gmail.com>
2025-11-16 15:08:55 +0100

    add generate

--- environments/golang/default.nix
+++ environments/golang/default.nix
@@ -8,6 +8,7 @@
     autocmd FileType go nnoremap <buffer> <CR>r :w<CR>:GoRun<CR>
     autocmd FileType go nnoremap <buffer> <CR>t :w<CR>:GoTest<CR>
     autocmd FileType go nnoremap <buffer> <CR>c :w<CR>:GoCoverage<CR>
+    autocmd FileType go nnoremap <buffer> <CR>gg :w<CR>:GoGenerate<CR>
     autocmd FileType go nnoremap <buffer> <leader>jj :w<CR>:GoAddTags<CR>
     autocmd FileType templ nnoremap <buffer> <CR> :w<CR>:!templ generate -f %<CR>
     augroup END
